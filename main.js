907fd2268496e0d61bf25eacbc5a51f30d47c425
Murad <murad.akverdiev@4px.ru>
2024-05-18 16:57:01 +0300

    upd

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async ()=> {
   const bingScript = document.querySelector('script[vk-app-binding]')
   const app = document.createElement('div') 
   app.setAttribute('id', 'app')
-  document.body.insertBefore(bingScript, app)
+  document.body.insertBefore(app, bingScript)
   const { createApp, defineAsyncComponent } = await import('vue')
   const App = defineAsyncComponent(()=> import('./src/App.vue'))
   createApp(App).mount(app)
