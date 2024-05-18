d03ed65ae96f2de9c46d4279ac42da2e8632f00c
Murad <murad.akverdiev@4px.ru>
2024-05-18 17:04:37 +0300

    upd

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', async ()=> {
-  const bingScript = document.querySelector('script[vk-app-binding]')
-  const app = document.createElement('div') 
-  app.setAttribute('id', 'app')
-  document.body.insertBefore(app, bingScript)
   const { createApp, defineAsyncComponent } = await import('vue')
   const App = defineAsyncComponent(()=> import('./src/App.vue'))
+
+  const app = appendAppContainer(app)
   createApp(App).mount(app)
+
+  function appendAppContainer(app) {
+    const bingScript = document.querySelector('script[vk-app-binding]')
+    const parent = bingScript.parentElement
+    const app = document.createElement('div') 
+    app.setAttribute('id', 'app')
+    parent.insertBefore(app, bingScript)
+    return app
+  }
 })
+
