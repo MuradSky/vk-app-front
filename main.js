1375a2762f57d458b561456ab6fc3d5f1c34f418
Murad <murad.akverdiev@4px.ru>
2024-05-18 16:41:44 +0300

    upd

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', async ()=> {
-  const styles = await fetch('choose-card-app.css').then(r => r.text())
-  const styleTag = document.createElement('style')
-  styleTag.appendChild(document.createTextNode(styles))
-  document.head.appendChild(styleTag)
+  // const styles = await fetch('choose-card-app.css').then(r => r.text())
+  // const styleTag = document.createElement('style')
+  // styleTag.appendChild(document.createTextNode(styles))
+  // document.head.appendChild(styleTag)
 
   const app = document.createElement('div') 
   app.setAttribute('id', 'app')
