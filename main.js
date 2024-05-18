b0ed988503820967b8fcd5a5cc82b57c762591a8
Murad <murad.akverdiev@4px.ru>
2024-05-18 16:53:55 +0300

    upd

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', async ()=> {
-  // const styles = await fetch('choose-card-app.css').then(r => r.text())
-  // const styleTag = document.createElement('style')
-  // styleTag.appendChild(document.createTextNode(styles))
-  // document.head.appendChild(styleTag)
-
+  const bingScript = document.querySelector('script[vk-app-binding]')
   const app = document.createElement('div') 
   app.setAttribute('id', 'app')
-  document.body.appendChild(app)
+  document.body.insertBefore(bingScript, app)
   const { createApp, defineAsyncComponent } = await import('vue')
   const App = defineAsyncComponent(()=> import('./src/App.vue'))
   createApp(App).mount(app)
